7e9745cbc3b39901c7e26f4bf3f3e6f5098c11ef
CadeSayner <cadesayner@gmail.com>
2024-10-19 18:45:20 +0200

    Added dog form

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,9 @@
+import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from "$env/static/private";
 import { turso } from "$lib/turso.server";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
+    console.log("env: ", TURSO_DATABASE_URL, TURSO_AUTH_TOKEN);
     const { rows } = await turso.execute("SELECT * FROM dogs");
     return {
         dogs: rows
--- src/routes/dogs/+page.svelte
+++ src/routes/dogs/+page.svelte
@@ -4,6 +4,26 @@
 </script>
 
 <form action="/dogs/create" method="post">
-    <input type="number" name="id" id="id">
+    Name
+    <input type="text" name="name" id="name"/>
+    <br>
+    Image
+    <input type="text" name="image"/>
+    <br>
+
+    Breed
+    <input type="text" name="breed"/>
+
+    <br>
+    Bio
+    <input type="text" name="bio"/>
+    <br>
+    Birth Date
+    <input type="date" name="date_birth" />
+    <br>
+
+    Date of Gruesome death
+    <input type="date" name="date_death"/>
+    <br>
     <input type="submit" value="Submit">
 </form>
