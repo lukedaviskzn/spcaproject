efab5100079255c4a12a8e4390de995c1685b450
CadeSayner <cadesayner@gmail.com>
2024-10-20 02:45:16 +0200

    bla

--- src/routes/dogs/create/+page.server.js
+++ src/routes/dogs/create/+page.server.js
@@ -4,23 +4,19 @@ import { turso } from "$lib/turso.server";
 /** @type {import('./$types').Actions} */
 export const actions = {
     default: async ({ request }) => {
-    
         let data = await request.formData();
         let name = data.get('name')?.toString();
         let type = data.get('type')?.toString();
         let image = data.get('image')?.toString();
-        let bio = data.get('bio')?.toString();
+        let bio = "";
         let breed = data.get('breed')?.toString();
         let date_of_birth = data.get('date_birth')?.toString();
         let date_of_death = data.get('date_death')?.toString();
         let hobby = data.get('hobby')?.toString();
 
-        console.log(bio);
         await fetch(`http://localhost:3000/generateBio?name=${name}&breed=${breed}&type=${type}&hobby=${hobby}`).then(response => {
-            return response.text()
-        }).then(dogdata => {bio = dogdata.toString()})
-        
-        console.log(bio);
+            return response.text();
+        }).then(dogdata => {bio = dogdata.toString()});
 
         if (data) {
             await turso.execute({
@@ -30,3 +26,5 @@ export const actions = {
         }
     },
 };
+
+
