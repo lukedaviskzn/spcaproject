64d5f4ce64e9a3cc0f807ffeccac3686f642c2db
Luke Davis <davislccg@gmail.com>
2024-10-20 06:09:34 +0200

    nonce

--- src/routes/thankyou/+page.server.js
+++ src/routes/thankyou/+page.server.js
@@ -1,6 +1,8 @@
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ params }) {
+export async function load({ url }) {
+    const nonce = url.searchParams.get('nonce');
+    const interact_ref = url.searchParams.get('interact_ref');
     return {
-        resp: await fetch(`http://127.0.0.1:3000/completePayment?nonce=${params.nonce}&interact_ref=${params.interact_ref}`),
+        resp: await fetch(`http://127.0.0.1:3000/completePayment?nonce=${nonce}&interact_ref=${interact_ref}`),
     };
 }
